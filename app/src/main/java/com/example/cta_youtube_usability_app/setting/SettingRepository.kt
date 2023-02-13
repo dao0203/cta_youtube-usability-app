6b410e9048776b51f5779b7439012e0272390d6f
dao0203 <youzaizuoteng872@gmail.com>
2023-02-13 12:12:30 +0900

    DataStoreのボタン配置（ButtonPosition）のデータを読み取る部分を実装

---
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/SettingRepository.kt
@@ -0,0 +1,31 @@
+package com.example.cta_youtube_usability_app.setting
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "com/example/cta_youtube_usability_app/setting")
+
+class SettingRepository (dataStore: DataStore<Preferences>){
+    val DEFINED_KEY = intPreferencesKey("defined_key")
+
+    val definedKeyFlow : Flow<Int> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException){
+                emit(emptyPreferences())
+            }else{
+                throw exception
+            }
+        }
+        .map {
+            preferences ->
+            preferences[DEFINED_KEY]?: 0
+        }
+}
