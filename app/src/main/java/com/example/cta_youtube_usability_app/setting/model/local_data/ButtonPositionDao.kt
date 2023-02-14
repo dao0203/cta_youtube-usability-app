eccb8cdacb890426e27f8fe125ee42981d936a81
dao0203 <youzaizuoteng872@gmail.com>
2023-02-14 15:02:47 +0900

    [clean]ButtonPositionのDAOに追加・削除・更新アノテーションを使用

---
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPositionDao.kt
@@ -0,0 +1,20 @@
+package com.example.cta_youtube_usability_app.setting.model.local_data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface ButtonPositionDao {
+    @Query("SELECT *FROM button_positions")
+    fun getAllButtonPosition(): Flow<List<ButtonPosition>>
+
+    @Insert
+    fun insertButtonPosition(buttonPosition: ButtonPosition)
+
+    @Update
+    fun updateButtonPosition(buttonPosition: ButtonPosition)
+
+    @Delete
+    fun deleteButtonPosition(buttonPosition: ButtonPosition)
+}
