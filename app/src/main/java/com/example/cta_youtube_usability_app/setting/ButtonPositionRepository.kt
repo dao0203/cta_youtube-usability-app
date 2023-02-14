dfe478b4cf52873d6787656419f4f8485564f234
dao0203 <youzaizuoteng872@gmail.com>
2023-02-14 15:24:31 +0900

    [add]ButtonPositionのレポジトリを追加

---
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/ButtonPositionRepository.kt
@@ -0,0 +1,36 @@
+package com.example.cta_youtube_usability_app.setting
+
+import androidx.annotation.WorkerThread
+import com.example.cta_youtube_usability_app.setting.model.local_data.ButtonPosition
+import com.example.cta_youtube_usability_app.setting.model.local_data.ButtonPositionDao
+import kotlinx.coroutines.flow.Flow
+
+class ButtonPositionRepository (private val buttonPositionDao: ButtonPositionDao){
+
+    //クエリを実行してButtonPositionの全てのデータを格納
+    val allButtonPositionData: Flow<List<ButtonPosition>>
+    = buttonPositionDao.getAllButtonPosition()
+
+
+    /** ボタンポジション追加メソッド **/
+    //わざわざ、非同期関数じゃなくても良い、という警告を無視してくれる
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread//WorkerThreadで動くように明示してあげる
+    suspend fun insertButtonPosition(buttonPosition: ButtonPosition){
+        buttonPositionDao.insertButtonPosition(buttonPosition)
+    }
+
+    /** ボタンポジション更新メソッド **/
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun updateButtonPosition(buttonPosition: ButtonPosition){
+        buttonPositionDao.updateButtonPosition(buttonPosition)
+    }
+
+    /** ボタンポジション削除メソッド **/
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteButtonPosition(buttonPosition: ButtonPosition){
+        buttonPositionDao.deleteButtonPosition(buttonPosition)
+    }
+}
