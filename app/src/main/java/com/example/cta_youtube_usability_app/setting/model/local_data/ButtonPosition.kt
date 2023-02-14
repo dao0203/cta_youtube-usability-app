1829afd55c6d1e95305efb7c9be8845823a9610a
dao0203 <youzaizuoteng872@gmail.com>
2023-02-15 01:28:45 +0900

    [clean]data classのコンストラクタ内は改行せずに統一

--- app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPosition.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPosition.kt
@@ -15,22 +15,16 @@ data class ButtonPosition(
     val layoutName: String,        //レイアウト名
     @ColumnInfo(name = "full_screen_x")
     val fullScreenX: Int,          //全画面ボタンのX座標
-
     @ColumnInfo(name = "full_screen_y")
     val fullScreenY: Int,          //全画面ボタンのY座標
-
     @ColumnInfo(name = "play_button_x")
     val playButtonX: Int,          //再生ボタンのX座標
-
     @ColumnInfo(name = "play_button_y")
     val playButtonY: Int,          //再生ボタンのY座標
-
     @ColumnInfo(name = "close_button_x")
     val closeButtonX: Int,         //動画閉じるボタンのX座標
-
     @ColumnInfo(name = "close_button_y")
     val closeButtonY: Int,         //動画閉じるボタンのY座標
-
     @ColumnInfo(name = "screen_orientation")
     val screenOrientation: Boolean //スマホの向き　false->横　true->縦
 )
