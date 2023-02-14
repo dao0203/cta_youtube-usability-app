0236b8c5f1390cc1dba1195db398aa6a2ca0159d
dao0203 <youzaizuoteng872@gmail.com>
2023-02-14 14:06:22 +0900

    [add]ButtonPositionsのDatabaseを作成

---
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/data/ButtonPositionDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.cta_youtube_usability_app.setting.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ButtonPosition::class], version = 1)
+abstract class ButtonPositionDatabase : RoomDatabase() {
+    abstract fun buttonPositionDao(): ButtonPositionDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ButtonPositionDatabase? = null
+
+        fun getDatabase(context: Context): ButtonPositionDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ButtonPositionDatabase::class.java,
+                    "button_position_database"
+                    ).build()
+                INSTANCE = instance
+                //return instance
+                instance
+            }
+        }
+
+    }
+}
