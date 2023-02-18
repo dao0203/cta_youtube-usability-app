fe30548717f894bf0e9f584932779cc49937dda8
dao0203 <youzaizuoteng872@gmail.com>
2023-02-18 15:17:20 +0900

    [add]動画閉じるボタンテーブルの追加

---
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/parent_entity/CloseVideoButton.kt
@@ -0,0 +1,19 @@
+package com.example.cta_youtube_usability_app.setting.model.local_data.parent_entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * データクラス：動画を閉じるボタン
+ */
+@Entity(tableName = "close_video_button")
+data class CloseVideoButton(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "cvb_id")
+    val CloseVideoButtonId: Int,  //動画閉じるボタンID
+    @ColumnInfo(name = "cvb_x")
+    val CloseVideoButtonX: Int,   //動画閉じるボタンのX座標
+    @ColumnInfo(name = "cvb_y")
+    val CloseVideoButtonY: Int    //動画閉じるボタンのY座標
+)
