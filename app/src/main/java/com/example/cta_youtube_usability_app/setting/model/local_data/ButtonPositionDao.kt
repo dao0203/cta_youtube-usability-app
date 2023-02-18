9cafc8f61db12e28a256a60b202a23b85c3ffdf7
dao0203 <youzaizuoteng872@gmail.com>
2023-02-18 20:56:44 +0900

    [update] Transactionを削除

--- app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPositionDao.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPositionDao.kt
@@ -5,19 +5,16 @@ import androidx.room.*
 @Dao
 interface ButtonPositionDao {
 
-    @Transaction
     @Query("SELECT * FROM button_position")
     suspend fun getAllButtonPosition(): List<ButtonPosition>
 
-    @Transaction
+
     @Insert
     suspend fun insertButtonPosition(buttonPosition: ButtonPosition)
 
-    @Transaction
     @Update
     suspend fun updateButtonPosition(buttonPosition: ButtonPosition)
 
-    @Transaction
     @Delete
     suspend fun deleteButtonPosition(buttonPosition: ButtonPosition)
 }
