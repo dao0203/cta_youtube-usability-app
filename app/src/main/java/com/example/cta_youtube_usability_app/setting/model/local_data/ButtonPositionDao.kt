a92c9e7019f54ff355379183b522e9c88f84b39f
dao0203 <youzaizuoteng872@gmail.com>
2023-02-20 01:38:18 +0900

    [clean] IDEの設定をし、ワイルドカードインポートを削除

--- app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPositionDao.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/model/local_data/ButtonPositionDao.kt
@@ -1,6 +1,10 @@
 package com.example.cta_youtube_usability_app.setting.model.local_data
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface ButtonPositionDao {
