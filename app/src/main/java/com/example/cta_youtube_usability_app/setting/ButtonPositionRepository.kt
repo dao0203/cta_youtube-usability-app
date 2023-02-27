2f6dcb4eefe364b001cfd66bf665e176efc1321a
dao0203 <youzaizuoteng872@gmail.com>
2023-02-27 09:19:53 +0900

    [update] suspend関数でButtonPosition全てのデータを返すように変更

--- app/src/main/java/com/example/cta_youtube_usability_app/setting/ButtonPositionRepository.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/ButtonPositionRepository.kt
@@ -5,32 +5,33 @@ import com.example.cta_youtube_usability_app.setting.model.local_data.ButtonPosi
 import com.example.cta_youtube_usability_app.setting.model.local_data.ButtonPositionDao
 import kotlinx.coroutines.flow.Flow
 
-class ButtonPositionRepository (private val buttonPositionDao: ButtonPositionDao){
+class ButtonPositionRepository(private val buttonPositionDao: ButtonPositionDao) {
 
-    //クエリを実行してButtonPositionの全てのデータを格納
-    val allButtonPositionData: Flow<List<ButtonPosition>>
-    = buttonPositionDao.getAllButtonPosition()
-
+    /** ボタンポジション取得メソッド **/
+    @WorkerThread
+    suspend fun getAllButtonPositionData(): List<ButtonPosition> {
+        return buttonPositionDao.getAllButtonPosition()
+    }
 
     /** ボタンポジション追加メソッド **/
     //わざわざ、非同期関数じゃなくても良い、という警告を無視してくれる
     @Suppress("RedundantSuspendModifier")
     @WorkerThread//WorkerThreadで動くように明示してあげる
-    suspend fun insertButtonPosition(buttonPosition: ButtonPosition){
+    suspend fun insertButtonPosition(buttonPosition: ButtonPosition) {
         buttonPositionDao.insertButtonPosition(buttonPosition)
     }
 
     /** ボタンポジション更新メソッド **/
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
-    suspend fun updateButtonPosition(buttonPosition: ButtonPosition){
+    suspend fun updateButtonPosition(buttonPosition: ButtonPosition) {
         buttonPositionDao.updateButtonPosition(buttonPosition)
     }
 
     /** ボタンポジション削除メソッド **/
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
-    suspend fun deleteButtonPosition(buttonPosition: ButtonPosition){
+    suspend fun deleteButtonPosition(buttonPosition: ButtonPosition) {
         buttonPositionDao.deleteButtonPosition(buttonPosition)
     }
 }
