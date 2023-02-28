9a38c378cde80894a591246797bdf3b317b787fc
dao0203 <youzaizuoteng872@gmail.com>
2023-02-28 10:49:32 +0900

    [update] sealedでUI状態を操作し、viewModelScope.launch内で実装

--- app/src/main/java/com/example/cta_youtube_usability_app/setting/ButtonPositionViewModel.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/ButtonPositionViewModel.kt
@@ -1,14 +1,15 @@
 package com.example.cta_youtube_usability_app.setting
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.cta_youtube_usability_app.setting.model.local_data.ButtonPositionEntity
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class ButtonPositionViewModel(private val repository: ButtonPositionRepository) : ViewModel() {
     companion object {
@@ -16,39 +17,57 @@ class ButtonPositionViewModel(private val repository: ButtonPositionRepository) 
         const val TAG = "ButtonPositionViewModel"
     }
 
+    //データベース操作をしているときはUIを一時的に操作できないようにする変数
+    //StateFlowでFragmentに値が変化したことを通知する
+    private val _isLoading: MutableStateFlow<UiState> = MutableStateFlow(UiState.Success)
+    val isLoading: StateFlow<UiState> = _isLoading
+
     val allButtonPositionData: StateFlow<List<ButtonPositionEntity>?> =
         flow {
-            //flow内でemitメソッドを使用して1つずつ送信される
-            emit(repository.getAllButtonPositionData())
+            _isLoading.value = UiState.IsLoading
+            try {//flow内でemitメソッドを使用して1つずつ送信される
+                emit(repository.getAllButtonPositionData())
+                _isLoading.value = UiState.Success
+            } catch (e: Exception) {
+                _isLoading.value = UiState.Error(e)
+            }
         }.stateIn(//stateInメソッドを使用してStateFlowに変換
             scope = viewModelScope,
-            started = SharingStarted.Eagerly,
+            started = SharingStarted.WhileSubscribed(),
             initialValue = null
         )
 
-    fun insertButtonPosition(buttonPosition: ButtonPositionEntity) = flow {
-        try {
-            //emit関数で値を送信することが出来る
-            emit(repository.insertButtonPosition(buttonPosition))
-        } catch (e: Exception) {
-            Log.e(TAG, "$e")
+    fun insertButtonPosition(buttonPosition: ButtonPositionEntity) {
+        viewModelScope.launch {
+            try {
+                _isLoading.value = UiState.IsLoading
+                repository.insertButtonPosition(buttonPosition)
+                _isLoading.value = UiState.Success
+            } catch (e: Exception) {
+                _isLoading.value = UiState.Error(e)
+            }
         }
     }
 
-    fun updateButtonPosition(buttonPosition: ButtonPositionEntity) = flow {
-        try {
-            emit(repository.updateButtonPosition(buttonPosition))
-        } catch (e: Exception) {
-            Log.e(TAG, "$e")
+    fun updateButtonPosition(buttonPosition: ButtonPositionEntity) {
+        viewModelScope.launch {
+            try {
+                _isLoading.value = UiState.IsLoading
+                repository.updateButtonPosition(buttonPosition)
+            } catch (e: Exception) {
+                _isLoading.value = UiState.Error(e)
+            }
         }
-
     }
 
-    fun deleteButtonPosition(buttonPosition: ButtonPositionEntity) = flow {
-        try {
-            emit(repository.deleteButtonPosition(buttonPosition))
-        } catch (e: Exception) {
-            Log.e(TAG, "$e")
+    fun deleteButtonPosition(buttonPosition: ButtonPositionEntity) {
+        viewModelScope.launch {
+            try {
+                _isLoading.value = UiState.IsLoading
+                repository.deleteButtonPosition(buttonPosition)
+            } catch (e: Exception) {
+                _isLoading.value = UiState.Error(e)
+            }
         }
     }
 }
@@ -63,4 +82,10 @@ class ButtonPositionViewModelFactory(private val repository: ButtonPositionRepos
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
+}
+
+sealed class UiState {
+    object Success : UiState()
+    data class Error(val exception: Throwable) : UiState()
+    object IsLoading : UiState()
 }
