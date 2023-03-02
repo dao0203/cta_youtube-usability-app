6c0b5e2a211bfd75cd38343cfba78f138f3cf217
dao0203 <youzaizuoteng872@gmail.com>
2023-03-02 13:02:48 +0900

    [add] DataStoreと横縦レイアウトIDのKeyの追加

--- app/src/main/java/com/example/cta_youtube_usability_app/DataStore.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/DataStore.kt
@@ -2,12 +2,14 @@ package com.example.cta_youtube_usability_app
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import java.util.prefs.Preferences
+
+//DataStoreをシングルトンとして扱えるようにする
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "layout")
 
 //横と縦のレイアウトIDを格納するKeyを宣言
-val LAND_SELECTED_LAYOUT_ID = stringPreferencesKey("landscape_selected_layout_id")
-val PORT_SELECTED_LAYOUT_ID = stringPreferencesKey("port_selected_layout_id")
+val LAND_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("land_selected_layout_id")
+val PORT_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("port_selected_layout_id")
