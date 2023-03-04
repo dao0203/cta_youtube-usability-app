8c147571548a53221376858de2266db648c7f6cb
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-04 22:29:43 +0900

    [clean] アプリケーション起動時にDataStoreに初期値を設定

--- app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.cta_youtube_usability_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.preferences.core.edit
+import androidx.lifecycle.lifecycleScope
 import com.example.cta_youtube_usability_app.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //アプリケーション起動時にDataStoreに初期値を設定（youtube_layout）
+        lifecycleScope.launch{
+            withContext(Dispatchers.IO){
+                dataStore.edit { layout ->
+                    layout[LAND_SELECTED_LAYOUT_ID_KEY] = "youtube_layout"
+                    layout[PORT_SELECTED_LAYOUT_ID_KEY] = "youtube_layout"
+                }
+            }
+        }
     }
 }
