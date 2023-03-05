7c4517f64bc72036da854376115528fadc442a94
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-05 16:25:51 +0900

    [add] IDの更新メソッドを追加

--- app/src/main/java/com/example/cta_youtube_usability_app/layoutIdRepository.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/layoutIdRepository.kt
@@ -1,17 +1,21 @@
 package com.example.cta_youtube_usability_app
 
 import android.content.Context
+import androidx.annotation.WorkerThread
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.example.cta_youtube_usability_app.setting.LandSelectedLayoutId
+import com.example.cta_youtube_usability_app.setting.PortSelectedLayoutId
 import kotlinx.coroutines.flow.first
 
 //DataStoreをシングルトンとして扱えるようにする
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "layout")
 
 //シングルトンとして扱えるようにするため、拡張関数を引数として持ってくる
-class LayoutIdRepository(private val context: Context){
+class SelectedLayoutIdRepository(private val context: Context) {
 
     //dataStoreのデータのやり取りを行うために宣言
     private val dataStore = context.dataStore
@@ -21,9 +25,28 @@ class LayoutIdRepository(private val context: Context){
     private val PORT_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("port_selected_layout_id")
 
     //横レイアウトID GETメソッド（nullだったら初期値はYouTubeレイアウトID）
-    suspend fun getLandSelectedId(): String = dataStore.data.first()[LAND_SELECTED_LAYOUT_ID_KEY] ?: "youtube_layout"
+    @WorkerThread
+    suspend fun getLandSelectedLayoutId(): String =
+        dataStore.data.first()[LAND_SELECTED_LAYOUT_ID_KEY] ?: "youtube_layout"
 
     //縦レイアウトID GETメソッド（nullだったら初期値はYouTubeレイアウトID）
-    suspend fun getPortSelectedId(): String = dataStore.data.first()[PORT_SELECTED_LAYOUT_ID_KEY] ?: "youtube_layout"
+    @WorkerThread
+    suspend fun getPortSelectedLayoutId(): String =
+        dataStore.data.first()[PORT_SELECTED_LAYOUT_ID_KEY] ?: "youtube_layout"
 
+    //横レイアウトID updateメソッド
+    @WorkerThread
+    suspend fun updateLandSelectedLayoutId(landLayoutId: LandSelectedLayoutId) {
+        dataStore.edit { layout ->
+            layout[LAND_SELECTED_LAYOUT_ID_KEY] = landLayoutId.landSelectedLayoutId
+        }
+    }
+
+    //縦レイアウトID updateメソッド
+    @WorkerThread
+    suspend fun updatePortSelectedLayoutId(portSelectedLayoutId: PortSelectedLayoutId) {
+        dataStore.edit { layout ->
+            layout[PORT_SELECTED_LAYOUT_ID_KEY] = portSelectedLayoutId.portSelectedLayoutId
+        }
+    }
 }
