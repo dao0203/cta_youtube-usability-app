7fdc45cf172905f5ea71d7516c9d3a55b77bcc19
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-05 16:07:48 +0900

    [add] それぞれのレイアウトIDをGETするメソッドを追加

--- app/src/main/java/com/example/cta_youtube_usability_app/layoutIdRepository.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/layoutIdRepository.kt
@@ -16,6 +16,14 @@ class LayoutIdRepository(private val context: Context){
     //dataStoreのデータのやり取りを行うために宣言
     private val dataStore = context.dataStore
 
-//横と縦のレイアウトIDを格納するKeyを宣言
-val LAND_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("land_selected_layout_id")
-val PORT_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("port_selected_layout_id")
+    //横と縦のレイアウトIDを格納するKeyを宣言
+    private val LAND_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("land_selected_layout_id")
+    private val PORT_SELECTED_LAYOUT_ID_KEY = stringPreferencesKey("port_selected_layout_id")
+
+    //横レイアウトID GETメソッド（nullだったら初期値はYouTubeレイアウトID）
+    suspend fun getLandSelectedId(): String = dataStore.data.first()[LAND_SELECTED_LAYOUT_ID_KEY] ?: "youtube_layout"
+
+    //縦レイアウトID GETメソッド（nullだったら初期値はYouTubeレイアウトID）
+    suspend fun getPortSelectedId(): String = dataStore.data.first()[PORT_SELECTED_LAYOUT_ID_KEY] ?: "youtube_layout"
+
+}
