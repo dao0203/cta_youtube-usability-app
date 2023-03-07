5a038cae1c1e8218287ad87f355e0d1ddc7725bc
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-07 23:38:21 +0900

    [update] 聞き手のレイアウトID名の変更

--- app/src/main/java/com/example/cta_youtube_usability_app/setting/LayoutId.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/LayoutId.kt
@@ -2,6 +2,6 @@ package com.example.cta_youtube_usability_app.setting
 
 enum class LayoutId(name: String) {
     YOUTUBE("youtube_layout"),
-    RIGHT("right_hand_layout"),
-    LEFT("left_hand_layout")
+    RIGHT("right_handed_layout"),
+    LEFT("left_handed_layout")
 }
