7645e06ed0a40f888e1d7a920487738052b01277
佐藤佑哉 <111327327+dao0203@users.noreply.github.com>
2023-03-09 20:30:45 +0900

    Revert "Revert "Feature/setting from model to view""

---
+++ app/src/main/java/com/example/cta_youtube_usability_app/setting/SelectedLayoutIdModel.kt
@@ -0,0 +1,4 @@
+package com.example.cta_youtube_usability_app.setting
+
+data class LandSelectedLayout(val landSelectedLayoutId: String)
+data class PortSelectedLayout(val portSelectedLayoutId: String)
