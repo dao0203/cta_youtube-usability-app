99e09ac371bc638006bf4b715c466366832ed0b0
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-10 22:58:06 +0900

    [clean] TODO：ボトムナビゲーションの機能の実装

--- app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
@@ -10,5 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //TODO: ボトムナビゲーションビューの設定
+//        val bottomNavigationView = binding.bottomNavigationView
+//        val navController =supportFragmentManager.findFragmentById(binding.navHostFragment.id)
+//        bottomNavigationView.setupWithNavController(navController!!.findNavController())
     }
 }
