a414592c86e664465641f562f790dad7741b5694
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-11 12:27:05 +0900

    [add] ボトムナビゲーションバーの実装

--- app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
@@ -2,17 +2,40 @@ package com.example.cta_youtube_usability_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import androidx.fragment.app.Fragment
+import androidx.media3.common.util.UnstableApi
 import com.example.cta_youtube_usability_app.databinding.ActivityMainBinding
+import com.example.cta_youtube_usability_app.movie.VideoFragment
+import com.example.cta_youtube_usability_app.setting.view.SettingFragment
 
 
+@UnstableApi
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //TODO: ボトムナビゲーションビューの設定
-//        val bottomNavigationView = binding.bottomNavigationView
-//        val navController =supportFragmentManager.findFragmentById(binding.navHostFragment.id)
-//        bottomNavigationView.setupWithNavController(navController!!.findNavController())
+
+        //アプリケーションが起動された時にビデオ画面に遷移するようにする
+        replaceFragment(VideoFragment())
+
+        //ボトムナビゲーションビューの設定
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.video_fragment -> replaceFragment(VideoFragment())
+                R.id.setting_fragment -> replaceFragment(SettingFragment())
+            }
+            true
+        }
+    }
+
+    // 引数のフラグメントを置き換えて画面に表示するメソッド
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.navHostFragment.id, fragment)
+            .commit()
     }
 }
