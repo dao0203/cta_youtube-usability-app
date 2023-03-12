06b20c907aec72c94b7a797b5559de7f07151f0a
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-12 14:07:10 +0900

    [delete] UnstableApiアノテーションの削除

--- app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.cta_youtube_usability_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.media3.common.util.UnstableApi
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cta_youtube_usability_app.databinding.ActivityMainBinding
 
-@UnstableApi
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
