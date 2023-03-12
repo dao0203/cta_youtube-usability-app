cfd052f707a09c0174fcfb0229cb33f9ca325542
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-12 13:58:36 +0900

    [update] setupWithNavControllerでボトムナビゲーションを設定

--- app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.cta_youtube_usability_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.media3.common.util.UnstableApi
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.cta_youtube_usability_app.databinding.ActivityMainBinding
-import com.example.cta_youtube_usability_app.movie.VideoFragment
-import com.example.cta_youtube_usability_app.setting.view.SettingFragment
 
 @UnstableApi
 class MainActivity : AppCompatActivity() {
@@ -17,20 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //ボトムナビゲーションビューの設定
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.video_fragment -> replaceFragment(VideoFragment())
-                R.id.setting_fragment -> replaceFragment(SettingFragment())
-            }
-            true
-        }
-    }
-
-    // 引数のフラグメントを置き換えて画面に表示するメソッド
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(binding.navHostFragment.id, fragment)
-            .commit()
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id)
+        val navController = navHostFragment?.findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController!!)
     }
 }
