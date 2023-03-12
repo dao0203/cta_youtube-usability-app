d14f1e255093eca2c1f27e57de774b91355e0422
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-12 13:32:38 +0900

    [delete] onCreate中のビデオ画面に遷移するメソッドを削除

--- app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/MainActivity.kt
@@ -2,13 +2,11 @@ package com.example.cta_youtube_usability_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import androidx.fragment.app.Fragment
 import androidx.media3.common.util.UnstableApi
 import com.example.cta_youtube_usability_app.databinding.ActivityMainBinding
 import com.example.cta_youtube_usability_app.movie.VideoFragment
 import com.example.cta_youtube_usability_app.setting.view.SettingFragment
-
 
 @UnstableApi
 class MainActivity : AppCompatActivity() {
@@ -17,9 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        //アプリケーションが起動された時にビデオ画面に遷移するようにする
-        replaceFragment(VideoFragment())
 
         //ボトムナビゲーションビューの設定
         binding.bottomNavigationView.setOnItemSelectedListener {
