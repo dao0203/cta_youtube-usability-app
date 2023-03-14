4eeca1cf73f5f1e8d3cc769cbf8f074131201546
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-15 01:10:05 +0900

    [update] ChooseControllerLayoutFragmentはLoadingFragmentに遷移することに専念

--- app/src/main/java/com/example/cta_youtube_usability_app/movie/ChooseControllerLayoutFragment.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/movie/ChooseControllerLayoutFragment.kt
@@ -5,20 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.example.cta_youtube_usability_app.databinding.FragmentChooseControllerLayoutBinding
-import com.example.cta_youtube_usability_app.setting.SelectedLayoutIdRepository
-import kotlinx.coroutines.launch
+
 
 class ChooseControllerLayoutFragment : Fragment() {
     private var binding: FragmentChooseControllerLayoutBinding? = null
-    private val chooseControllerLayoutViewModel: ChooseControllerLayoutViewModel by viewModels {
-        ChooseControllerLayoutViewModelFactory(SelectedLayoutIdRepository(requireContext()))
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,18 +18,6 @@ class ChooseControllerLayoutFragment : Fragment() {
         binding = FragmentChooseControllerLayoutBinding.inflate(layoutInflater)
         return binding!!.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.CREATED) {
-                chooseControllerLayoutViewModel.getEachSelectedLayoutId()
-                chooseControllerLayoutViewModel.chooseControllerLayoutUiState.collect { value ->
-                }
-            }
-        }
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
