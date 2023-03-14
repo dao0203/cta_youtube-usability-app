c3af84d9278da36f5dcf42b7412b2100ba199887
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-15 01:09:00 +0900

    [add] Loading Fragmentが3つのレイアウトを操作するように実装

--- app/src/main/java/com/example/cta_youtube_usability_app/movie/operated_fragment/LoadingFragment.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/movie/operated_fragment/LoadingFragment.kt
@@ -5,15 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
 import com.example.cta_youtube_usability_app.R
+import com.example.cta_youtube_usability_app.databinding.FragmentLoadingBinding
+import com.example.cta_youtube_usability_app.movie.ChooseControllerLayoutUiState
+import com.example.cta_youtube_usability_app.movie.ChooseControllerLayoutViewModel
+import com.example.cta_youtube_usability_app.movie.ChooseControllerLayoutViewModelFactory
+import com.example.cta_youtube_usability_app.setting.LayoutId
+import com.example.cta_youtube_usability_app.setting.SelectedLayoutIdRepository
+import kotlinx.coroutines.launch
 
 class LoadingFragment : Fragment() {
+    private var binding: FragmentLoadingBinding? = null
+    private val chooseControllerLayoutViewModel: ChooseControllerLayoutViewModel by viewModels {
+        ChooseControllerLayoutViewModelFactory(SelectedLayoutIdRepository(requireContext()))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loading, container, false)
+        binding = FragmentLoadingBinding.inflate(inflater)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = view.findNavController()
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                chooseControllerLayoutViewModel.getEachSelectedLayoutId()
+                chooseControllerLayoutViewModel.chooseControllerLayoutUiState.collect { value ->
+                    when (value) {
+                        is ChooseControllerLayoutUiState.Loading -> {
+
+                        }
+                        is ChooseControllerLayoutUiState.Success -> {
+                            // FIXME:Successきてるし、ViewModelからのデータの受け取りも成功してるど、画面遷移しない
+                            when (value.portSelectedLayout.portSelectedLayoutId) {
+                                LayoutId.YOUTUBE.name -> navController.navigate(R.id.action_loadingFragment_to_youTubeLayoutFragment)
+                                LayoutId.LEFT_HANDED.name -> navController.navigate(R.id.action_loadingFragment_to_leftHandedLayoutFragment)
+                                LayoutId.RIGHT_HANDED.name -> navController.navigate(R.id.action_loadingFragment_to_rightHandedLayoutFragment)
+                            }
+                        }
+                        is ChooseControllerLayoutUiState.Error -> {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
