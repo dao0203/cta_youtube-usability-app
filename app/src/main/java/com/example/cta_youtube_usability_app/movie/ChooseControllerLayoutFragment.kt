8605526903371ae3d776190e4d145b0a45830426
佐藤佑哉 <yuyudevelop0203@gmail.com>
2023-03-20 08:43:35 +0900

    [update] Loadingフラグメントに遷移するよう実装

--- app/src/main/java/com/example/cta_youtube_usability_app/movie/ChooseControllerLayoutFragment.kt
+++ app/src/main/java/com/example/cta_youtube_usability_app/movie/ChooseControllerLayoutFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.cta_youtube_usability_app.R
 import com.example.cta_youtube_usability_app.databinding.FragmentChooseControllerLayoutBinding
 
 
@@ -17,6 +19,11 @@ class ChooseControllerLayoutFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentChooseControllerLayoutBinding.inflate(layoutInflater)
         return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(R.id.action_chooseControllerLayoutFragment_to_loadingFragment)
     }
     override fun onDestroyView() {
         super.onDestroyView()
